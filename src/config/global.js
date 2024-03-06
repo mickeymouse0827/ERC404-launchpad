8d7911e41cc0a781a521d6979c125b82126deed9
mickeymouse0827 <darestef2@gmail.com>
2024-03-06 09:32:20 +0900

    added by mickeymouse0827

---
+++ src/config/global.js
@@ -0,0 +1,43 @@
+import { mainnet, goerli, bsc, bscTestnet, arbitrum, arbitrumGoerli, polygon, polygonMumbai, zkSync, zkSyncTestnet } from "wagmi/chains";
+
+export const IS_PRODUCT_MODE = true // TODO
+
+// export const launchpad_contract_address = {
+//     mainnet: "0x4F8B7885E970291847283d18511C9c9d50646fD5",
+//     testnet: "0x429f54cc1c2dfe0d250b03ad83fd83a5f58b9905",
+// }
+
+export const launchpad_contract_address_mainnet = {
+    mainnet: "0x4F8B7885E970291847283d18511C9c9d50646fD5",
+    bsc: "0xE0B073079407ebF6a9B5af0e9304A1f7eee7259e",
+    arbitrum: "0xE0B073079407ebF6a9B5af0e9304A1f7eee7259e",
+    polygon: "0xE0B073079407ebF6a9B5af0e9304A1f7eee7259e",
+    zkSync: "0x3a0246F245Cc54a71f6AaE3850eFF6D2B520977a"
+}
+
+export const launchpad_contract_address_testnet = {
+    goerli: "0x429f54cc1c2dfe0d250b03ad83fd83a5f58b9905",
+    bscTestnet: "",
+    arbitrumGoerli: "",
+    polygonMumbai: "",
+    // zkSyncTestnet: "0x7DB7eD06a68F5d97b7D8cE98BAbC116C82bAc27D"
+    zkSyncTestnet: "0x89a6F3486447b167fCAA4c4e24B52A533f539089"
+}
+
+export const mainnetChains = [mainnet, bsc, arbitrum, polygon, zkSync]
+
+export const testnetChains = [goerli, bscTestnet, arbitrumGoerli, polygonMumbai, zkSyncTestnet]
+
+export const global = {
+    chain: IS_PRODUCT_MODE ? mainnetChains : testnetChains,
+    // chain: IS_PRODUCT_MODE ? mainnetChains : testnetChains,
+    launchpad_contract_address: IS_PRODUCT_MODE ? launchpad_contract_address_mainnet : launchpad_contract_address_testnet,
+    PROJECT_ID: 'c278a3db8eed101935395863b318fff7',
+    PROJECT: 'Launchpad',
+    Decimals: 18
+    // defaultGas: IS_PRODUCT_MODE ? 0.0005 : 0.0004,
+    // REFETCH_INTERVAL: 30000,
+    // REFETCH_SECOND: 1000,
+    // IUniswapV2Router01Address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    // WethContractAddress: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+}
---
+++ src/pages/dapp/tokens/create-token/BasicInfo.jsx
@@ -0,0 +1,305 @@
+import { useState, useContext, useEffect } from "react";
+import SelectChainRadio from "../../../../components/SelectChainRadio";
+import FloatingLabelInput from "../../../../components/inputs/FloatingLabelInput";
+
+import { formDataContext } from "../../../../contexts/formDataContext";
+import { useNetwork } from "wagmi";
+import { IS_PRODUCT_MODE } from "../../../../config/global";
+import { toast } from "react-toastify";
+
+const BasicInfo = () => {
+  const { chain } = useNetwork();
+  const [chainType, setChainType] = useState('');
+  const { formData, setFormData } = useContext(formDataContext)
+  const testFlag = IS_PRODUCT_MODE;
+
+  useEffect(() => {
+    if (chain) {
+      if (testFlag === false) {
+        if (chain.id === 280)
+          setChainType('zkSyncTestnet')
+        else if (chain.id === 5)
+          setChainType('goerli')
+      } else if (testFlag === true) {
+        if (chain.id === 1)
+          setChainType('mainnet')
+        else if (chain.id === 56)
+          setChainType('bsc')
+        else if (chain.id === 42161)
+          setChainType('arbitrum')
+        else if (chain.id === 137)
+          setChainType('polygon')
+        else if (chain.id === 324)
+          setChainType('zkSync')
+      }
+    }
+  }, [chain]);
+
+  const handler = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleRadioClick = async (type) => {
+    if (testFlag === false) {
+      if (type === 'zkSyncTestnet') {
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0x118' }], // chainId must be in hexadecimal numbers
+            });
+            setChainType(type)
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      } else if (type === 'goerli') {
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0x5' }], // chainId must be in hexadecimal numbers
+            });
+            setChainType(type)
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      }
+    } else if (testFlag === true) {
+      if (type === 'mainnet') {
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0x1' }], // chainId must be in hexadecimal numbers
+            });
+            setChainType(type)
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      } else if (type === 'bsc') {
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0x38' }], // chainId must be in hexadecimal numbers
+            });
+            setChainType(type)
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      } else if (type === 'arbitrum') {
+        setChainType(type)
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0xa4b1' }], // chainId must be in hexadecimal numbers
+            });
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      } else if (type === 'polygon') {
+        setChainType(type)
+        if (window.ethereum) {
+          try {
+            // Request to switch to the Binance Smart Chain network
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0x89' }], // chainId must be in hexadecimal numbers
+            });
+          } catch (error) {
+            // Handle the error
+            console.error(error);
+          }
+        } else {
+          // MetaMask is not installed
+          alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        }
+      } else if (type === 'zkSync') {
+        // setChainType(type)
+        // if (window.ethereum) {
+        //   try {
+        //     // Request to switch to the Binance Smart Chain network
+        //     await window.ethereum.request({
+        //       method: 'wallet_switchEthereumChain',
+        //       params: [{ chainId: '0x144' }], // chainId must be in hexadecimal numbers
+        //     });
+        //   } catch (error) {
+        //     // Handle the error
+        //     console.error(error);
+        //   }
+        // } else {
+        //   // MetaMask is not installed
+        //   alert('MetaMask is not installed. Please consider installing it: MetaMask [^2^]');
+        // }
+        toast.warn("Coming soon!")
+      }
+    }
+    // setChainType(type)
+  }
+
+  return (
+    <div className="flex justify-start items-center flex-col w-full gap-4">
+      <div className="grid mx-auto items-start max-w-[1300px] mt-2 grid-cols-1 lg:grid-cols-1 w-full gap-5 lg:gap-10">
+        <div className="flex justify-start items-start flex-col gap-3 w-full abc bg-black rounded-xl p-5">
+          <div className="mb-3">
+            <h4 className="text-white text-2xl font-semibold">
+              Select Your Token Network:
+            </h4>
+          </div>
+          {
+            testFlag === false &&
+            <SelectChainRadio
+            img="/eth.png"
+            name="goerli"
+            label="goerli"
+            border
+            sub="ERC-404"
+            selected={chainType == 'goerli'}
+            handleRadioClick={() => handleRadioClick('goerli')}
+            />
+          }
+          {
+            testFlag === false &&
+            <SelectChainRadio
+              img="/zkicon.png"
+              name="zkSyncTestnet"
+              label="zkSyncTestnet"
+              border
+              sub="ERC-404"
+              selected={chainType == 'zkSyncTestnet'}
+              handleRadioClick={() => handleRadioClick('zkSyncTestnet')}
+            />
+          }
+          {
+            testFlag &&
+            <SelectChainRadio
+              img="/eth.png"
+              name="Ethereum"
+              label="eth"
+              border
+              sub="ERC-404"
+              selected={chainType == 'mainnet'}
+              handleRadioClick={() => handleRadioClick('mainnet')}
+            />
+          }
+          {testFlag &&
+            <SelectChainRadio
+              border
+              img="/Binancelogo.svg"
+              name="Binance Smart Chain"
+              sub="BEP-404"
+              label="bsc"
+              selected={chainType == 'bsc'}
+              handleRadioClick={() => handleRadioClick('bsc')}
+            />
+          }
+          {testFlag &&
+            <SelectChainRadio
+              img="/arbitrum.webp"
+              name="Arbitrum"
+              sub="ERC-404"
+              border
+              label="arbitrum"
+              selected={chainType == 'arbitrum'}
+              handleRadioClick={() => handleRadioClick('arbitrum')}
+            />
+          }
+          {testFlag &&
+            <SelectChainRadio
+              img="polygonicon.png"
+              name="Polygon (Matic)"
+              label="polygon"
+              border
+              sub="ERC-404"
+              selected={chainType == 'polygon'}
+              handleRadioClick={() => handleRadioClick('polygon')}
+            />
+          }
+          {testFlag &&
+            <SelectChainRadio
+              img="/zkicon.png"
+              name="zkSync"
+              border
+              label="zk"
+              sub={"ERC-404"}
+              selected={chainType == 'zkSync'}
+              handleRadioClick={() => handleRadioClick('zkSync')}
+            />
+          }
+        </div>
+        <div className="flex justify-start items-start flex-col gap-5 sm:gap-8 w-full abc bg-black rounded-xl p-5">
+          <FloatingLabelInput
+            handler={handler}
+            name="name"
+            placeholder="Ex:Ethereum"
+            bgColor="abc bg-black"
+            type="text"
+            value={formData.name}
+            topLabel="Token Name"
+          />
+          <FloatingLabelInput
+            handler={handler}
+            name="symbol"
+            placeholder="Ex: ETH"
+            bgColor="abc bg-black"
+            type="text"
+            value={formData.symbol}
+            topLabel="Token Symbol"
+          />
+          <FloatingLabelInput
+            handler={handler}
+            name="decimals"
+            placeholder="Ex: 18"
+            bgColor="abc bg-black"
+            type="number"
+            value={formData.decimals}
+            topLabel="Token Decimals"
+          />
+          <FloatingLabelInput
+            handler={handler}
+            name="supply"
+            placeholder="Ex: 100000000"
+            bgColor="abc bg-black"
+            type="number"
+            value={formData.supply}
+            topLabel="Token Supply"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BasicInfo;
